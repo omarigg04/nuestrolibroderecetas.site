4f375737cb7f334cba8bcd74abc8837123b9e70e
omarigg04 <gaxiolaomar04@gmail.com>
2024-07-26 19:51:34 -0600

    "Removed theme property from favicon objects and reduced number of favicon sizes, consolidating light and dark themes into a single set of icons."

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,36 +2,19 @@ import type {Favicon} from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: '/favicon/favicon-light-32.png',
-        theme: 'light',
+        src: '/favicon/favicon-16x16.png',
+        sizes: '16x16',
+    }, 
+    {
+        src: '/favicon/favicon-32x32.png',
         sizes: '32x32',
-    }, {
-        src: '/favicon/favicon-light-128.png',
-        theme: 'light',
-        sizes: '128x128',
-    }, {
-        src: '/favicon/favicon-light-180.png',
-        theme: 'light',
-        sizes: '180x180',
-    }, {
-        src: '/favicon/favicon-light-192.png',
-        theme: 'light',
+    }, 
+    {
+        src: '/favicon/favicon-192x192.png',
         sizes: '192x192',
-    }, {
-        src: '/favicon/favicon-dark-32.png',
-        theme: 'dark',
-        sizes: '32x32',
-    }, {
-        src: '/favicon/favicon-dark-128.png',
-        theme: 'dark',
-        sizes: '128x128',
-    }, {
-        src: '/favicon/favicon-dark-180.png',
-        theme: 'dark',
-        sizes: '180x180',
-    }, {
-        src: '/favicon/favicon-dark-192.png',
-        theme: 'dark',
-        sizes: '192x192',
+    }, 
+    {
+        src: '/favicon/favicon-512x512.png',
+        sizes: '512x512',
     }
-]
+];
